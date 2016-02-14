5b938b96c481074208dc9d8eb6a38fff42b0167c
ekstroem <github@ekstroem.com>
2016-02-15 00:14:07 +0100

    Two plots

--- ui.R
+++ ui.R
@@ -33,11 +33,11 @@ shinyUI(pageWithSidebar(
     mainPanel(
         h3(textOutput("caption")),
 
-        h4(verbatimTextOutput("Sample densities"))
+        h4(verbatimTextOutput("Sample densities")),
         verbatimTextOutput("The plot below shows the densities for the two sample means for comparison between two Gaussian populations with samples of same size and same standard deviation. The SMD blue line below shows the difference between the two population means."),
         plotOutput("dataPlot"),
 
-        h4(verbatimTextOutput("Test statistic densities"))
+        h4(verbatimTextOutput("Test statistic densities")),
         verbatimTextOutput("The plot below shows the densities for the two sample means for comparison between two Gaussian populations with samples of same size and same standard deviation. The SMD blue line below shows the difference between the two population means."),
         plotOutput("mpgPlot")
         )
